b08b74277e84bd0fbc87b0274532f6b741d612b2
João Paulo <jpjcjbr@gmail.com>
2012-10-08 22:37:11 -0300

    versao inicial da lib para oauth

---
+++ gen/com/wareninja/android/opensource/oauth2login/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wareninja.android.opensource.oauth2login;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
